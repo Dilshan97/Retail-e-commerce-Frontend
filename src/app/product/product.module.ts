2ab533db3f0e783957890706b4be85d946cca41b
Dilshan97 <dilshanramesh81@gmail.com>
2021-04-15 20:45:39 +0530

    product view component added

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -4,15 +4,17 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { ProductComponent } from './product.component';
 import { productRoutingModule } from './product.routing';
 import { ViewProductComponent } from './view-product/view-product.component';
+import { SingleProductComponent } from './single-product/single-product.component';
 
 const components = [
     ProductComponent,
-    ViewProductComponent
+    ViewProductComponent,
+    SingleProductComponent
 ];
 
 @NgModule({
     declarations: [
-        components
+        components   
     ],
     imports: [
         productRoutingModule,
